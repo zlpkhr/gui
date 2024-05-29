18cdda10a170bba673f6e2d5b70b719df1a336a7
Azamat Zulpykhar <spender.poised-0q@icloud.com>
2024-05-29 08:48:17 +0500

    Added script for sampling domains

---
+++ scripts/sample.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const { parseArgs } = require("node:util");
+const { createReadStream, writeFileSync } = require("node:fs");
+const { resolve } = require("node:path");
+const { createInterface } = require("node:readline");
+
+const options = {
+  input: { type: "string", short: "i" },
+  output: { type: "string", short: "o" },
+  size: { type: "string", short: "s" },
+};
+
+const args = parseArgs({ options });
+
+const readStream = createReadStream(resolve(process.cwd(), args.values.input));
+
+const readLine = createInterface({
+  input: readStream,
+  crlfDelay: Infinity,
+});
+
+const size = parseInt(args.values.size, 10);
+const reservoir = [];
+let count = 0;
+
+readLine.on("line", (line) => {
+  count += 1;
+
+  if (reservoir.length < size) {
+    reservoir.push(line);
+  } else {
+    const randomIndex = Math.floor(Math.random() * count);
+
+    if (randomIndex < size) {
+      reservoir[randomIndex] = line;
+    }
+  }
+});
+
+readLine.on("close", () => {
+  writeFileSync(
+    resolve(process.cwd(), args.values.output),
+    reservoir.join("\n"),
+  );
+});
