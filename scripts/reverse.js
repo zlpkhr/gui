0976ce39e9574a7273ccfe839493f49deec93aff
Azamat Zulpykhar <spender.poised-0q@icloud.com>
2024-05-29 08:29:31 +0500

    Added script for reversing domains

---
+++ scripts/reverse.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const { parseArgs } = require("node:util");
+const { Transform } = require("node:stream");
+const { createReadStream, createWriteStream } = require("node:fs");
+const { resolve } = require("node:path");
+const { createInterface } = require("node:readline");
+
+const options = {
+  input: { type: "string", short: "i" },
+  output: { type: "string", short: "o" },
+};
+
+const args = parseArgs({ options });
+
+const reverse = (domain) => domain.split(".").reverse().join(".");
+
+const transformer = new Transform({
+  transform(chunk, encoding, callback) {
+    const parts = chunk.toString().split("\t");
+    this.push(reverse(parts[1]).concat("\n"));
+    callback();
+  },
+});
+
+const readStream = createReadStream(resolve(__dirname, args.values.input), {
+  encoding: "utf8",
+});
+
+const readLine = createInterface({
+  input: readStream,
+  crlfDelay: Infinity,
+});
+
+readLine.on("line", (line) => {
+  transformer.write(line);
+});
+
+readLine.on("close", () => {
+  transformer.end();
+});
+
+const writeStream = createWriteStream(resolve(__dirname, args.values.output), {
+  encoding: "utf8",
+});
+
+transformer.pipe(writeStream);
